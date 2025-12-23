460701963a06424256af9585d5caa8a44a6ae1cd
tachothai524 <tachothai_th@cmu.ac.th>
2025-12-23 17:10:31 +0700

    lab10 finish

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int number = 0 ;
+    float sum1 = 0;
+    float sum2 = 0;
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    while(getline(source,textline)){
+        sum1 += atof(textline.c_str());
+        sum2 += pow(atof(textline.c_str()),2);
+        number++;
+    }
+    float A = sum1/number;
+    cout << "Number of data = " << number << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << A << '\n';
+    cout << "Standard deviation = " << sqrt((sum2/number)-pow(A,2));
+}
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
